5ca63f97ce729257e36699be255a70235253ae5c
Mohammad Ali <forrest482@outlook.com>
2024-02-22 15:32:39 +0330

    add images to blog post

---
+++ toystore/toystore/local_settings.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-sq7*2f(3nl6d)i#2)1)b^s&k9+dte17$9d@mvhyi_((did7&(='
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+ADMIN_PATH = 'admin/'
