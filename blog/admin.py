f3b7ff037163d8e9db1537d346b6a80d2c9a3204
Mohammad Ali <forrest482@outlook.com>
2024-02-24 20:21:05 +0330

    add thumbnail to blog category and post

--- blog/admin.py
+++ blog/admin.py
@@ -1,20 +1,42 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Category, Post, Comment, Media
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title',)
+    list_display = ('id', 'title', 'thumbnail_preview')
     search_fields = ('title',)
+
+    def thumbnail_preview(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="{}" style="width: 50px; height:50px;" />'.format(obj.thumbnail.url))
+        return ""
+
+    thumbnail_preview.short_description = "Thumbnail Preview"
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at')
+    list_display = ('id', 'title', 'category',
+                    'created_at', 'thumbnail_preview')
     list_filter = ('created_at', 'category')
     search_fields = ('title', 'content')
     date_hierarchy = 'created_at'
     prepopulated_fields = {'slug': ('title',)}
+    inlines = [CommentInline]
+
+    def thumbnail_preview(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="{}" style="width: 50px; height:50px;" />'.format(obj.thumbnail.url))
+        return ""
+
+    thumbnail_preview.short_description = "Thumbnail Preview"
 
 
 @admin.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
+    thumbnail = models.ImageField(
+        upload_to='category_thumbnails/', blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -16,13 +18,21 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
+    thumbnail = models.ImageField(
+        upload_to='post_thumbnails/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
-    # def get_images(self):
-    #     return self.media.
+    def get_images(self):
+        return self.media.filter(media_type='IMG')
+
+    def get_videos(self):
+        return self.media.filter(media_type='VID')
+
+    def get_audios(self):
+        return self.media.filter(media_type='AUD')
 
 
 class Comment(models.Model):
@@ -38,14 +48,18 @@ class Comment(models.Model):
 
 
 class Media(models.Model):
-    class MediaType(models.TextChoices):
-        IMAGE = 'IMG', _('Image')
-        VIDEO = 'VID', _('Video')
-        AUDIO = 'AUD', _('Audio')
+    MEDIA_TYPE_CHOICES = (
+        ('IMG', _('Image')),
+        ('VID', _('Video')),
+        ('AUD', _('Audio')),
+    )
 
     post = models.ForeignKey(Post, related_name='media',
                              on_delete=models.CASCADE)
     media_type = models.CharField(
-        max_length=3, choices=MediaType.choices, default=MediaType.IMAGE)
+        max_length=3,
+        choices=MEDIA_TYPE_CHOICES,
+        default='IMG'
+    )
     file = models.FileField(upload_to='blog_media/')
     caption = models.CharField(max_length=255, blank=True)
